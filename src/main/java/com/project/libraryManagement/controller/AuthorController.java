d8667aee5bb0bbc7729e03c40e4e490e52279409
Mifta <miftakhul348@gmail.com>
2024-12-05 21:04:03 +0700

    test commit

--- src/main/java/com/project/libraryManagement/controller/AuthorController.java
+++ src/main/java/com/project/libraryManagement/controller/AuthorController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/authors")
